dd7c6b5691fd0ec001383e7124f60f58da24bfc1
mateusCoder <mateus.moraes0507@gmail.com>
2022-03-30 13:06:01 -0300

    Created Project

---
+++ src/main/java/compass/politicalParty/PoliticalParty/PoliticalPartyApplication.java
@@ -0,0 +1,13 @@
+package compass.politicalParty.PoliticalParty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticalPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/compass/politicalParty/PoliticalParty/PoliticalPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package compass.politicalParty.PoliticalParty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticalPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
