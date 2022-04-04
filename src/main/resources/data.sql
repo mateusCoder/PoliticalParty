f7ebe5936d97d8cfd12b08863675f0388868553b
mateusCoder <mateus.moraes0507@gmail.com>
2022-04-03 23:57:43 -0300

    Change SQL datas and Add Postman Collection

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,18 @@
-INSERT INTO Associate(name, political_office, date, gender) VALUES ('Mateus', 'PREFEITO', '2022-03-30', 'MASCULINO');
-INSERT INTO Associate(name, political_office, date, gender) VALUES ('LULA', 'PRESIDENTE', '2022-03-30', 'MASCULINO');
-INSERT INTO Associate(name, political_office, date, gender) VALUES ('Manuela', 'SENADOR', '2022-03-30', 'FEMININO');
 INSERT INTO Associate(name, political_office, date, gender) VALUES
+('Mateus', 'PREFEITO', '2022-03-30', 'MASCULINO'),
+('LULA', 'PRESIDENTE', '2022-03-30', 'MASCULINO'),
+('Manuela', 'SENADOR', '2022-03-30', 'FEMININO'),
 ('Gilberto Nascimento JR', 'VEREADOR', '1967-10-25', 'MASCULINO'),
 ('Robert Willians', 'PREFEITO', '1983-07-12', 'MASCULINO'),
 ('Maria Dolores da Cunha', 'DEPUTADO_ESTADUAL', '1999-05-17', 'FEMININO'),
-('Danilo Schudeller', 'DEPUTADO_FEDERAL', '1943-02-28', 'MASCULINO'),
-('Gustavo Salvato', 'SENADOR', '1978-11-15', 'MASCULINO'),
-('Luciana Silva', 'GOVERNADOR', '1988-12-05', 'FEMININO'),
-('Gustavo Betete', 'PRESIDENTE', '1999-05-12', 'MASCULINO');
+('Daniela', 'DEPUTADO_FEDERAL', '1943-02-28', 'FEMININO'),
+('Lurdes', 'SENADOR', '1978-11-15', 'FEMININO'),
+('Freixo', 'GOVERNADOR', '1988-12-05', 'MASCULINO'),
+('Clayton', 'PRESIDENTE', '1999-05-12', 'MASCULINO');
+('Robson', 'PRESIDENTE', '1999-05-12', 'MASCULINO');
 
-INSERT INTO Political_Party(name, acronym, ideology, date) VALUES ('Partido do Trabalhadores', 'PT', 'ESQUERDA', '2022-04-01');
-INSERT INTO Political_Party(name, acronym, ideology, date) VALUES ('Partido Socialista', 'PSOL', 'ESQUERDA', '2022-04-01');
 INSERT INTO Political_Party(name, acronym, ideology, date) VALUES
+('Partido Socialista', 'PSOL', 'ESQUERDA', '2022-04-01'),
 ('Partido da Social Democracia Brasileira', 'PSDB', 'CENTRO', '1988-06-25'),
 ('Partido dos Trabalhadores', 'PT', 'ESQUERDA', '1980-02-10'),
 ('Movimento Democrático Brasileiro', 'MDB', 'DIREITA', '1981-06-30'),
